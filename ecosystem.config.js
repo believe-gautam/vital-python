46e7f667e6da886d50722192b4da3892389bffc8
root <root@vmi2310866.contaboserver.net>
2024-12-10 04:30:22 -0600

    server changes

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'flask-app',
+        script: '/var/www/html/vital-projects/vital-python/venv/bin/gunicorn', // Full path to gunicorn
+        args: 'run:app --workers 3 --bind 0.0.0.0:5000', // Adjust if needed for your app
+        interpreter: '/usr/bin/python3', // Use Python to execute Gunicorn
+        cwd: '/var/www/html/vital-projects/vital-python',  // Your app directory
+        env: {
+          FLASK_APP: 'run.py',  // or your entry file
+          FLASK_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
